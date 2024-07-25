08b804286dd89fdb84c2d989dd35fa442e8ae13d
Dan Lew <danlew42@gmail.com>
2024-07-25 15:27:43 -0500

    Added workaround for missing archival LE data

--- src/main/java/net/danlew/displate/main.kt
+++ src/main/java/net/danlew/displate/main.kt
@@ -1,7 +1,11 @@
 package net.danlew.displate
 
 import net.danlew.displate.Data.variants
+import net.danlew.displate.model.Author
 import net.danlew.displate.model.DualDisplates
+import net.danlew.displate.model.Edition
+import net.danlew.displate.model.Image
+import net.danlew.displate.model.Images
 import net.danlew.displate.model.LimitedDisplate
 import net.danlew.displate.model.LimitedType
 import net.danlew.displate.model.LimitedType.lumino
@@ -17,17 +21,73 @@ import java.nio.file.Files
 import java.nio.file.Paths
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalTime
 import kotlin.io.path.exists
 
 fun main() {
   val displateData = gatherLimitedEditionData() + Api.queryLuminos()!!
+  val displateIds = displateData.map { it.itemCollectionId }.toSet()
+  val archivalDataToUse = loadArchivalDisplateData().filter { it.limited.itemCollectionId !in displateIds }
+  val dualDisplateData = gatherNormalEditions(displateData) + archivalDataToUse
 
-  val dualDisplateData = gatherNormalEditions(displateData)
   val orderedDualDisplateData = orderDisplates(dualDisplateData)
   val csvData = displatesToCsvData(orderedDualDisplateData)
   outputToCsv(csvData)
 
   downloadImages(displateData)
+}
+
+// Displate removed a bunch of LEs from their database; we use archival data to restore them
+fun loadArchivalDisplateData(): List<DualDisplates> {
+  val reader = Thread.currentThread()
+    .contextClassLoader
+    .getResourceAsStream("old-displates.csv")
+    .bufferedReader()
+
+  val records = CSVFormat.Builder
+    .create(CSVFormat.DEFAULT)
+    .setHeader()
+    .setSkipHeaderRecord(true)
+    .build()
+    .parse(reader)
+
+  return records.map { record ->
+    val quantity = record["Quantity"]
+    val author = Author(
+      fullName = record["Artist"],
+      url = record["Artist Link"]
+    )
+
+    var normal: NormalDisplate? = null
+    if (!record["Normal Name"].isNullOrEmpty()) {
+      normal = NormalDisplate(
+        itemCollectionId = record["Normal Link"].substring(30).toInt(),
+        title = record["Normal Name"],
+        imageUrl = record["Normal Image"],
+        author = author
+      )
+    }
+
+    DualDisplates(
+      limited = LimitedDisplate(
+        title = record["Name"],
+        edition = Edition(
+          startDate = LocalDateTime.of(LocalDate.parse(record["Release Date"]), LocalTime.MIDNIGHT),
+          size = if (quantity == "N/A") 0 else quantity.toInt(),
+          type = LimitedType.parse(record["Type"])
+        ),
+        images = Images(
+          main = Image(
+            url = record["Image"]
+          )
+        ),
+        itemCollectionId = record["ID"].toInt(),
+        url = record["Link"].substring(20),
+        author = author
+      ),
+      normal = normal
+    )
+  }
 }
 
 fun gatherLimitedEditionData(): List<LimitedDisplate> {
--- src/main/java/net/danlew/displate/model/LimitedType.kt
+++ src/main/java/net/danlew/displate/model/LimitedType.kt
@@ -3,5 +3,11 @@ package net.danlew.displate.model
 enum class LimitedType(val type: String) {
   standard("LE"),
   ultra("ULE"),
-  lumino("Lumino"),
+  lumino("Lumino");
+
+  companion object {
+    fun parse(type: String): LimitedType {
+      return LimitedType.values().first { it.type == type }
+    }
+  }
 }
